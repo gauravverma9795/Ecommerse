515e6d3cf2191608495c3592117d9b875c9c2914
gauravverma9795 <gauravverma9795@gmail.com>
2024-06-16 04:09:39 +0530

    update

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productRoutes from "./routes/productRoutes.js";
 import cors from "cors";
 import path from "path"
+import { fileURLToPath } from 'url';
 
 //configure env
 dotenv.config();
@@ -19,22 +20,30 @@ connectDB();
 //rest object
 const app = express();
 
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 //middelwares
 app.use(cors());
 app.use(express.json());
 app.use(morgan("dev"));
 
-app.use(express.static(path.join(__dirname, './client/build')));
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+
 
 //routes
 app.use("/api/v1/auth", authRoutes);
 app.use("/api/v1/category", categoryRoutes);
 app.use("/api/v1/product", productRoutes);
 
-app.use('*',function(req, res){
-  res.sendFile(path.join(__dirname, './client/build/index.html'));
+// Example API endpoint
+app.get('/api/example', (req, res) => {
+  res.json({ message: 'Hello from API!' });
 });
 
+// Serve React app for all other routes
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client/build/index.html'));
+});
 
 
 //PORT
